1b30edce248e5e90ac3c7d3aed40170f026dacb2
Sagleft <sagleft@mfc.click>
2021-11-17 22:16:19 +0200

    add get trade history method

--- history.go
+++ history.go
@@ -1,1 +1,28 @@
 package uexchange
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+// GetTradeHistory - get trading history by pairs
+func (c *Client) GetTradeHistory(pairSymbol string) (*TradeHistoryDataContainer, error) {
+	body, err := c.sendRequest(c.getAPIURL("history/trade"), mapTable{
+		"pair": pairSymbol,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// decode response
+	var response APITradeHistoryResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, errors.New("failed to decode request response: " + err.Error())
+	}
+
+	if !response.Success {
+		return nil, errors.New("failed to get trade history") // TODO
+	}
+	return &response.Result, nil
+}
