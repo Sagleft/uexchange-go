3507ecbf943d9cf6b78642f4e838cc52724500d4
Sagleft <sagleft@mfc.click>
2021-12-09 20:01:18 +0200

    check response conversion

--- balance.go
+++ balance.go
@@ -12,6 +12,13 @@ func (c *Client) GetBalance() ([]BalanceData, error) {
 		return nil, err
 	}
 
+	// validate response
+	responseStr := string(body)
+	firstResponseChar := responseStr[0:1]
+	if firstResponseChar != "{" && firstResponseChar != "[" {
+		return nil, errors.New(responseStr)
+	}
+
 	// decode response
 	var response APIBalanceResponse
 	err = json.Unmarshal(body, &response)
